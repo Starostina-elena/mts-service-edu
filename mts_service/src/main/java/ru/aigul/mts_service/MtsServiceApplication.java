93b8e1fe2796226a1ddfe2fbd1405ff23816d40d
Aigul <aigul@niuitmo.ru>
2026-02-15 16:29:31 +0300

    Initial commit

---
+++ mts_service/src/main/java/ru/aigul/mts_service/MtsServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.aigul.mts_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtsServiceApplication.class, args);
+    }
+
+}
---
+++ mts_service/src/test/java/ru/aigul/mts_service/MtsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aigul.mts_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
