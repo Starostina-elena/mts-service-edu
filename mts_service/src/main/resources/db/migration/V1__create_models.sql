a7db560dd3a1f8e7f9b60a0f905fc5f2961a7cb4
Elena Starostina <starostina.lena05@gmail.com>
2026-03-07 01:09:20 +0300

    user password hash not null (migration

--- mts_service/src/main/resources/db/migration/V1__create_models.sql
+++ mts_service/src/main/resources/db/migration/V1__create_models.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS users (
   id BIGSERIAL PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(100) NOT NULL,
-  password_hash VARCHAR(255),
+  password_hash VARCHAR(255) NOT NULL,
   role role_enum NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
