a159a33e525cfb3bf55aab6a51072c03a7fcce4d
Aigul <aigul@niuitmo.ru>
2026-03-11 17:29:35 +0300

    lab1

--- mts_front/src/auth/mockUsers.js
+++ mts_front/src/auth/mockUsers.js
@@ -1,5 +1,5 @@
 export const MOCK_USERS = [
-  { id: 1, name: 'Иван Петров', role: 'USER' },
-  { id: 2, name: 'Мария Сидорова', role: 'MANAGER' },
-  { id: 3, name: 'Админ Системы', role: 'ADMIN' },
+  { id: 1, name: 'Тестовый Пользователь', role: 'USER' },
+  { id: 2, name: 'Администратор', role: 'ADMIN' },
+  { id: 3, name: 'Менеджер', role: 'MANAGER' },
 ]
